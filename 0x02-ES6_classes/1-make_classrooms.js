38112da8364a9bbb3251e92f60177b2ed39959f9
oumoukhalil <zsarr2021@gmail.com>
2024-05-31 18:01:57 +0000

    1. Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,12 @@
-import ClassRoom from './0-classroom';
-export default function initializeRooms() {
-	const classe1 = new ClassRoom(19);
-	const classe2 =new ClassRoom(20);
-	const classe3 = new ClassRoom(34);
-	const arr =[classe1,classe2,classe3]
-	return arr;
-}
 
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+
+  const arr = [class1, class2, class3];
+
+  return arr;
+}
