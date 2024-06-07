8b3722abd1d73eb747aad8436dc2d8c56f854214
oumoukhalil <fatsarr628@gmail.com>
2024-06-07 18:29:30 +0000

    2. Extending the Teacher class

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,67 @@
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+	workFromHome(): string {
+		return "Working from home";
+	}
+	getCoffeeBreak(): string {
+		return "Getting a coffee break";
+	}
+	workDirectorTasks(): string {
+		return "Getting to director tasks"
+	}
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return "Cannot work from home";
+	}
+	getCoffeeBreak(): string {
+		return "Connot have a break";
+	}
+	workTeacherTasks(): string {
+		return "Getting to work";
+	}
+}
+
+function createEmployee(salary: number | string ): Director | Teacher {
+	if (typeof salary === 'number' &&  salary < 500 ) {
+		return (new Teacher())
+	} else {
+		return (new Director())
+	}
+}
+
+function isDirector(employee: any): employee is Director {
+	return (employee instanceof Director)
+}
+
+function executeWork(employee: any): any {
+	const teacher = new Teacher();
+	const director = new Director();
+	if (isDirector(employee)) {
+		return director.workDirectorTasks();
+	} else {
+		return teacher.workTeacherTasks();
+	}
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects):string {
+	if(todayClass === 'Math') {
+		return "Teaching Math";
+	} else if(todayClass === 'History') {
+		return "Teaching History";
+	}
+}
